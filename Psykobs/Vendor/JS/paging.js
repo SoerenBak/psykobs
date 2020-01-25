7f29603329feafe764cfb1aa6d6503cd08d1b006
SoerenBak <34088652+SoerenBak@users.noreply.github.com>
2020-01-25 21:20:32 +0100

    start

---
+++ Psykobs/Vendor/JS/paging.js
@@ -0,0 +1,11 @@
+﻿$('#bs4-slide-carousel').carousel({
+    interval: 7000
+});
+
+$('#btnPrev').on('click', function () {
+    $('#bs4-slide-carousel').carousel('prev');
+});
+
+$('#btnNext').on('click', function () {
+    $('#bs4-slide-carousel').carousel('next');
+});
